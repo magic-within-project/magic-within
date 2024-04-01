57a31295ee226ffa6583859361c0feb52e3c2c89
LittleNa1000 <naphanccnp@gmail.com>
2024-04-01 20:17:40 +0700

    chore: install fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import "~/styles/globals.css";
 
-import { IBM_Plex_Sans_Thai_Looped } from "next/font/google";
+import { Charmonman, IBM_Plex_Sans_Thai_Looped } from "next/font/google";
 import { cn } from "../lib/utils";
 
 const ibmPlexSansThaiLooped = IBM_Plex_Sans_Thai_Looped({
   weight: ["400", "500", "600", "700"],
-  subsets: ["thai", "latin"],
+  subsets: ["thai"],
   variable: "--ibm-plex-sans-thai-looped-font",
+});
+
+const charmonman = Charmonman({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--charmonman-font",
 });
 
 export const metadata = {
@@ -24,8 +30,9 @@ export default function RootLayout({
     <html lang="th">
       <body
         className={cn(
-          "relative mx-auto min-h-screen w-full max-w-md overscroll-none font-sans",
+          "font-ibmPlex relative mx-auto min-h-screen w-full max-w-md overscroll-none",
           ibmPlexSansThaiLooped.variable,
+          charmonman.variable,
         )}
       >
         {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    fontFamily: {
+      ibmPlex: ["var(--ibm-plex-sans-thai-looped-font)", "sans-serif"],
+      charmonman: ["var(--charmonman-font)", "serif"],
+    },
     extend: {
-      fontFamily: {
-        sans: ["var(--ibm-plex-sans-thai-looped-font)", "sans-serif"],
-      },
       colors: {
         default: "#272018",
         active: "#B78181",
