edb5a3143fa1043a9a68a0118c38914dbd3011aa
LittleNa1000 <naphanccnp@gmail.com>
2024-04-01 21:10:18 +0700

    feat: /1-1

---
+++ src/app/(interactive)/1-1/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <Link
+        href="/1-2"
+        className="absolute h-full w-full bg-cover bg-no-repeat"
+      >
+        <motion.div
+          className="absolute right-[4vw] top-[18vh] text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1, delay: 1 } }}
+        >
+          <p>
+            และแล้ววันนี้ก็มาถึง...
+            <br />
+            <br />
+            วันที่คุณเฝ้ารอคอยอย่างใจจดใจจ่อ
+            <br />
+            วันที่คุณจะได้เริ่มต้นการเดินทางครั้งสำคัญ
+            <br />
+            ผู้คนรุ่นราวคราวเดียวกันจำนวนมากรอบตัว
+            <br />
+            ทำให้คุณรู้สึกประหม่าขึ้นมาเล็กน้อย
+            <br />
+          </p>
+        </motion.div>
+      </Link>
+    </>
+  );
+}
--- src/lib/bg-config.ts
+++ src/lib/bg-config.ts
@@ -1,6 +1,14 @@
 export const backgroundMapConfig = {
   "0": {
-    image: "/img/0.webp",
-    imagePreload: ["/img/0-1.webp"],
+    image: "/bg/1-2.webp",
+    imagePreload: ["/bg/1-1.webp"],
+  },
+  "1-1": {
+    image: "/bg/1-1.webp",
+    imagePreload: ["/bg/1-2.webp"],
+  },
+  "1-2": {
+    image: "/bg/1-2.webp",
+    imagePreload: [],
   },
 } as const;
