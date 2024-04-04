ef5860e8ceb55090e783aa325c34c266968c7e0f
matikant <fe-2003@hotmail.com>
2024-04-04 13:24:05 +0700

    feat: add localStorage

--- src/app/(interactive)/0-4/page.tsx
+++ src/app/(interactive)/0-4/page.tsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 
 export default function Page() {
   const [name, setName] = useState<string>("");
+  function handleName(name: string) {
+    setName(name);
+    localStorage.setItem("name", name);
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-cover bg-no-repeat text-center">
@@ -16,7 +20,7 @@ export default function Page() {
         id="name"
         className="bg-mwWhite absolute right-[14vw] top-[48.5vh] h-[2.5vh] w-[40.67vw] resize-none overflow-hidden pl-1 pt-0.5 text-sm"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => handleName(e.target.value)}
       />
       <Link href="/0-5" className="absolute bottom-[28vh]">
         <button className="text-mwWhite h-[4.5vh] w-[22vw] rounded-lg bg-purple font-semibold drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
