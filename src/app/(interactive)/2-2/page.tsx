a526a9e487b0eeececf93804999912ce67e04018
LittleNa1000 <naphanccnp@gmail.com>
2024-04-12 03:40:38 +0700

    fix: scene 2 merge conflict

--- src/app/(interactive)/2-2/page.tsx
+++ src/app/(interactive)/2-2/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 1, delay: 1 } }}
-          className="absolute inset-x-0 top-[20%] bg-white-radial bg-center bg-no-repeat p-10"
+          className="bg-img-white-radial absolute inset-x-0 top-[20%] bg-center bg-no-repeat p-10"
         >
           <p className="text-center">
             ตอนนี้คาบเรียนแรกของคุณ
