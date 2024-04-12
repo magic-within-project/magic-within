79caf3d76b7ae29462f2c1a9260ac8d6eff20145
LittleNa1000 <naphanccnp@gmail.com>
2024-04-12 12:28:39 +0700

    refactor: sort bg-config.ts

--- src/lib/bg-config.ts
+++ src/lib/bg-config.ts
@@ -283,74 +283,6 @@ export const backgroundMapConfig = {
     image: "/bg/3-1.webp",
     imagePreload: [],
   },
-  "6-10": {
-    image: "/bg/bg6-10.webp",
-    imagePreload: [],
-  },
-  "6-11": {
-    image: "/bg/bg6-10.webp",
-    imagePreload: [],
-  },
-  "6-12": {
-    image: "/bg/bg6-10.webp",
-    imagePreload: [],
-  },
-  "6-13": {
-    image: "/bg/bg6-10.webp",
-    imagePreload: [],
-  },
-  "6-14": {
-    image: "/bg/bg6-10.webp",
-    imagePreload: [],
-  },
-  "6-15": {
-    image: "/bg/bg6-10.webp",
-    imagePreload: ["/bg/bg6-16.webp"],
-  },
-  "6-16": {
-    image: "/bg/bg6-16.webp",
-    imagePreload: ["/bg/bg6-17.webp"],
-  },
-  "6-17": {
-    image: "/bg/bg6-17.webp",
-    imagePreload: ["/bg/bg6-18.webp"],
-  },
-  "6-18": {
-    image: "/bg/bg6-18.webp",
-    imagePreload: ["/bg/bg6-18-1.webp","/bg/bg6-18-2.webp"],
-    
-  },
-  "6-19": {
-    image: ["/bg/bg6-19-1.webp","/bg/bg6-19-2.webp"],
-    stopMotionDuration: 2000,
-    imagePreload: ["/bg/bg6-20.webp"],
-    
-  },
-  "6-20": {
-    image: "/bg/bg6-20.webp",
-    imagePreload: [],
-    
-  },
-  "6-21": {
-    image: "/bg/bg6-20.webp",
-    imagePreload: [],
-    
-  },
-  "6-22": {
-    image: "/bg/bg6-20.webp",
-    imagePreload: ["/bg/bg6-23.webp"],
-    
-  },
-  "6-23": {
-    image: "/bg/bg6-23.webp",
-    imagePreload: [],
-    
-  },
-  "6-24": {
-    image: "/bg/bg6-23.webp",
-    imagePreload: [],
-    
-  },
   "6-1": {
     image: "/img/6-1.webp",
     imagePreload: ["/img/6-1.webp"],
@@ -385,6 +317,67 @@ export const backgroundMapConfig = {
   },
   "6-9": {
     image: "/img/6-6.webp",
+    imagePreload: [],
+  },
+  "6-10": {
+    image: "/bg/bg6-10.webp",
+    imagePreload: [],
+  },
+  "6-11": {
+    image: "/bg/bg6-10.webp",
+    imagePreload: [],
+  },
+  "6-12": {
+    image: "/bg/bg6-10.webp",
+    imagePreload: [],
+  },
+  "6-13": {
+    image: "/bg/bg6-10.webp",
+    imagePreload: [],
+  },
+  "6-14": {
+    image: "/bg/bg6-10.webp",
+    imagePreload: [],
+  },
+  "6-15": {
+    image: "/bg/bg6-10.webp",
+    imagePreload: ["/bg/bg6-16.webp"],
+  },
+  "6-16": {
+    image: "/bg/bg6-16.webp",
+    imagePreload: ["/bg/bg6-17.webp"],
+  },
+  "6-17": {
+    image: "/bg/bg6-17.webp",
+    imagePreload: ["/bg/bg6-18.webp"],
+  },
+  "6-18": {
+    image: "/bg/bg6-18.webp",
+    imagePreload: ["/bg/bg6-18-1.webp", "/bg/bg6-18-2.webp"],
+  },
+  "6-19": {
+    image: ["/bg/bg6-19-1.webp", "/bg/bg6-19-2.webp"],
+    stopMotionDuration: 2000,
+    imagePreload: ["/bg/bg6-20.webp"],
+  },
+  "6-20": {
+    image: "/bg/bg6-20.webp",
+    imagePreload: [],
+  },
+  "6-21": {
+    image: "/bg/bg6-20.webp",
+    imagePreload: [],
+  },
+  "6-22": {
+    image: "/bg/bg6-20.webp",
+    imagePreload: ["/bg/bg6-23.webp"],
+  },
+  "6-23": {
+    image: "/bg/bg6-23.webp",
+    imagePreload: [],
+  },
+  "6-24": {
+    image: "/bg/bg6-23.webp",
     imagePreload: [],
   },
   "6-25": {
