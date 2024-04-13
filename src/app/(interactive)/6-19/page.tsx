8972829410597d612ccdf503c9afbe1d350a7fae
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2024-04-13 16:14:35 +0700

    6-19

--- src/app/(interactive)/6-19/page.tsx
+++ src/app/(interactive)/6-19/page.tsx
@@ -13,7 +13,7 @@ export default function page() {
             >
                 <Link href="6-20" className="">
                     <div className="grid justify-items-center h-screen w-1024">
-                    <h1 className="absolute justify-self-center bottom-16 animate-pulse">แตะเพื่ออ่านจดหมาย</h1>
+                    <h1 className="absolute justify-self-center bottom-16 left-[130px] animate-pulse">แตะเพื่ออ่านจดหมาย</h1>
                     </div>
                 </Link>
 
